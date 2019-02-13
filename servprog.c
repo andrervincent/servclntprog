5675a5a2bb8c20df58d4d072fb85029c43a76d59
André Vincent <andrevncnt7@gmail.com>
2019-02-12 21:50:09 -0500

    Adds connection handler, listener, adds backlog macrodefinition

--- servprog.c
+++ servprog.c
@@ -8,8 +8,9 @@
 #include <stdio.h>
 
 /* Global constants */
-#define ECHO_PORT		(2002)
-#define MAX_LINE		(1000)		
+#define ECHO_PORT		(2005)
+#define MAX_LINE		(1000)	
+#define LISTENQ			(3)	
 
 int main(int argc, char * argv[]) {
 	int list_s, conn_s;
@@ -21,7 +22,23 @@ int main(int argc, char * argv[]) {
 	sockaddr.sin_family = AF_INET;
 	sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	sockaddr.sin_port = htons(ECHO_PORT);
+	// Creates socket to receive requests
+	if ((list_s = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("Socket not created \n");
+	}
+	// binds socket to an address
+	if (bind(list_s, (struct sockaddr *) &sockaddr, sizeof(sockaddr)) < 0) {
+		printf("Socket not bound.\n");
+	}
+	// Listens for connection requests
+	if (listen(list_s, LISTENQ) < 0) {
+		printf("Socket not listening\n");
+	}
 
-	
-
+	while (1) {
+		// Handles connections
+		if ((conn_s = accept (list_s, NULL, NULL)) < 0) {
+			printf("connection not accepted");
+		}
+	}
 }
